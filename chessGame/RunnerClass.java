7db4768b9fe503baf82f7bc851e038c2507e0f2b
Vaughn <vaughnjparker@gmail.com>
2017-05-17 23:59:24 -0400

    Just some tweaks to RunnerClass.java

--- chessGame/RunnerClass.java
+++ chessGame/RunnerClass.java
@@ -3,30 +3,32 @@ import java.util.Scanner;
 public class RunnerClass
 {
     public static void main (String[] args) {
-
-        Scanner playerInput = new Scanner(System.in);
         
-        char[][] chessBoard = {
+        Scanner playerInput = new Scanner(System.in);
+
+        /*char[][] chessBoard = {
             {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
             {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-            {' ', ' ', ' ', 'P', ' ', ' ', ' ', ' '},  
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
             {' ', ' ', ' ', ' ', 'N', ' ', ' ', ' '},
             {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
             {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
             {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
             {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
             };
-        BoardState gameBoard = new BoardState(chessBoard, true);
+        BoardState gameBoard = new BoardState(chessBoard, true);*/
+        BoardState gameBoard = new BoardState();
+        char[][] currentPosition = gameBoard.getPosition();
+        System.out.println(gameBoard);
+        
         MoveFinder ai = new MoveFinder();
-        
-        //System.out.println(chessBoard);
-        for (int i = 0; i < chessBoard.length; i++) {
-            for (int j = 0; j < chessBoard[i].length; j++) {
-                System.out.print(chessBoard[i][j] + " ");
-            }
-            System.out.println();
-        }
-        
         System.out.println(ai.possibleMoves(gameBoard));
+        
+        
+        
+        
+        System.out.println();
+        
+        System.out.println(gameBoard.makeMove("4644 "));
     }
-}   
+}
