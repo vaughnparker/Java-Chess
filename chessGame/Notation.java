b35c94d77c605e9349bb95d4e31d99275eded136
Vaughn <vaughnjparker@gmail.com>
2017-05-24 23:43:26 -0400

    Made gameplay more fluid, introducing Notation class (ill do that later if i have time)
    Tomorrow I'm focusing on minimax

---
+++ chessGame/Notation.java
@@ -0,0 +1,10 @@
+public class Notation
+{
+    public static String convertCrazyToAlgebraic(BoardState boardToSearch, String crazyMove) {
+        return "";
+    }
+    
+    public static String convertAlgebraicToCrazy(BoardState boardToSearch, String crazyMove) {
+        return "";
+    }
+}
